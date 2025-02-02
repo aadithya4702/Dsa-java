4b7201452c55df9bdd49f1bb5ebbe7e4d7f90690
aadithya4702 <aadithyaprasad111@gmail.com>
2025-02-02 15:39:38 +0530

    Added compress and decompress and prime number check

---
+++ JavaTest/PrimeCheck.java
@@ -0,0 +1,24 @@
+package JavaTest;
+
+public class PrimeCheck {
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false; // 0 and 1 are not prime numbers
+        }
+        for (int i = 2; i <= n / 2; i++) { // Check up to sqrt(n)
+            if (n % i == 0) {
+                return false; // If divisible by any number, not prime
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 10; // Example number
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
---
+++ JavaTest/compress.java
@@ -0,0 +1,47 @@
+public class compress {
+
+    public static void main(String[] args) {
+
+        String a = "aaabbaaccc";
+        char checkChar = a.charAt(0);
+        int currCount = 1;
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 1; i < a.length(); i++) {
+            if (checkChar == a.charAt(i)) {
+                currCount++;
+            } else {
+                str.append(checkChar);
+                str.append(currCount);
+                checkChar = a.charAt(i);
+                currCount = 1;
+            }
+
+        }
+        str.append(checkChar).append(currCount);
+
+        System.out.println("Compressesed string of " + a + " is : " + str.toString());
+        revertToNormal(str.toString());
+    }
+
+    private static void revertToNormal(String string) {
+
+        int count = 0;
+        char curChar = ' ';
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) {
+                count = Character.getNumericValue(string.charAt(i)); // Correct conversion
+
+                for (int j = 0; j < count; j++) {
+                    str.append(curChar);
+                }
+            } else {
+                curChar = string.charAt(i);
+            }
+        }
+        System.out.println("After decompressing from " + string + " to " + str.toString());
+
+    }
+}
